653cd3cbd4bc4ef41a1fc603c9075f4581ac1f19
gs <gs@DESKTOP-7QK4U9T>
2025-06-26 16:07:00 +0700

    Calc done

--- FileName.cpp
+++ FileName.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+void Calc()
+{
+    int a = 10;
+    int b = 7;
+    std::cout << "A = " << a << '\n';
+    std::cout << "B = " << b << '\n';
+    std::cout << "A + B = " << a + b << '\n';
+    std::cout << "A - B = " << a - b << '\n';
+    std::cout << "A * B = " << a * b << '\n';
+    std::cout << "A / B = " << a / b << '\n';
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Calc();
 }
