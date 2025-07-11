d396ef04197b4254e13aae55aabbaa1fc7fdac7d
PyroTehnik <maks.potylitsun@gmail.com>
2025-07-12 00:24:56 +0700

    min list sorter DONE

--- FileName.cpp
+++ FileName.cpp
@@ -44,7 +44,7 @@ void RandomN()
             std::cout << "You lose" << '\n';
             return;
         }
-    }   
+    }
 }
 void RPS()
 {
@@ -55,12 +55,12 @@ void RPS()
         std::string weapon[] = {"rock","paper","scissors"}; 
 
         // create results
-        std::string result[3][3]{ 
+        std::string result[3][3]{
             {"draw","win","lose"},
             {"lose","draw","win"},
             {"win","lose","draw"}
         };
-        
+
         std::string userStr;
         int user = -1;
         int enemy = rand() % 2;
@@ -68,11 +68,11 @@ void RPS()
         std::cin >> userStr;
 
         // selection check & assigment "user" value on which was confirmed choice
-        bool correct = false; 
+        bool correct = false;
         for (int i = 0; i < 3; i++)
         {
             if (userStr == weapon[i]) {
-                correct = true;               
+                correct = true;
                 user = i;
             }
         }
@@ -84,34 +84,23 @@ void RPS()
         else
         {
             //appeal to std::string result to choose the result
-            std::cout << result[enemy][user] << '\n'; 
+            std::cout << result[enemy][user] << '\n';
         }
-    }       
+    }
 }
 
 
 void Store()
 {
-    struct product {
-        string name;
-        int price;
-        int quantity;
-    };
+    
 
-    vector<product>shop;
-    shop.push_back({ "banana",10,9 });
-    shop.push_back({ "apple",8,7 });
-    shop.push_back({ "lemon",6,5 });
-
-    cout << "How much money do you have?" << '\n';
-    int money;
-    cin >> money;
-
+    /*
+    
     cout << "Shopping list for maximum spending money:" << '\n';
-    //for (int a = 0; a < 3; a++)
-    //{;
-    //cout << "max: " << *max_element(begin(shop[a].price), end(shop[a].price)) << '\n';
-    //}
+    for (int a = 0; a < 3; a++)
+    {;
+    cout << "max: " << *max_element(begin(shop[a].price), end(shop[a].price)) << '\n';
+    }
     {
         int maxwell = -1;
         for (int a = 0; a < shop.size(); a++)
@@ -121,10 +110,9 @@ void Store()
         std::cout << "max price is: " << maxwell << std::endl;
         }
 
-    cout << "Maximum quantity purchase list"<<'\n';
-
-    /*
-    for (int i = 0; i < shop.size(); i++) {
+    cout << "Maximum quantity purchase list" << '\n';*/
+    
+   /* for (int i = 0; i < shop.size(); i++) {
         cout << i+1 << " " << shop[i].name << " price " << shop[i].price << " quantity " << shop[i].quantity<< '\n';
     }
 
@@ -177,13 +165,76 @@ void Store()
         else {
             return;
         }
-    }
-    */
+    }*/
 }
-
-
 
 int main()
 {
-    Store();
+    struct product {
+        string name;
+        int price;
+        int quantity;
+    };
+
+    vector<product>shop;
+    shop.push_back({ "banana",10,9 });
+    shop.push_back({ "apple",8,7 });
+    shop.push_back({ "lemon",6,5 });
+
+    cout << "How much money do you have?" << '\n';
+    int money;
+    cin >> money;
+
+
+    int c = 0;
+    int minPrice = money;
+    cout << money << '\n';
+    cout << minPrice << '\n';
+    // DO цикл должен работать пока денег больше или равно чем самый дешевый продукт на складе в момент проверки условия цикла
+    // - но если его запустить сейчас так он будет бесконечно выдавать coutы потому что не может переключиться на след дешевый продукт
+    for (int m = 0; money >= minPrice; m++) {
+
+        cout << shop[c].price << '\t' << shop[c].quantity << '\n';
+
+        //? почему после выкупа первой самой дешевой позиции он не ищет новую позицию?
+
+        // находим продукт с минимальной ценой
+        int i = 0;
+        minPrice = money;
+        --minPrice;
+        cout << shop[0].price << '\t' << shop[0].quantity << '\n';
+        cout << shop[1].price << '\t' << shop[1].quantity << '\n';
+        cout << shop[2].price << '\t' << shop[2].quantity << '\n';
+        for (int i = 0; i < 3; i++) {
+            cout << shop[i].price << '\t' << shop[i].quantity << '\n';
+            cout << minPrice << '\n';
+            if (minPrice >= shop[i].price && shop[i].quantity != 0) {
+                minPrice = shop[i].price;
+                c = i;
+            }
+        }
+        //? как вынести значение за пределы цикла не выдавая его новой переменной?
+        cout << c << '\n';
+
+        // выкупаем его пока его не останется
+        if (shop[c].quantity != 0) {
+
+            cout << "min price is: " << minPrice << '\n';
+            cout << "price belongs: " << shop[c].name << '\n';
+
+            for (; money >= minPrice && shop[c].quantity != 0;) {
+                if (money >= minPrice) {
+                    money -= minPrice;
+                    shop[c].quantity -= 1;
+                    cout << "Money: " << money << '\n';
+                    cout << shop[c].name << " quantity: " << shop[c].quantity << '\n';
+                }
+            }
+        }
+        if (shop[c].quantity == 0) {
+            shop[c].price = 100;
+        }
+        cout << minPrice << '\n';
+        cout << money << '\n';
+    }
 }
